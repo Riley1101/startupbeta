f731dd8b8cf881480fe95a34f6ed94cc21520add
Riley1101 <www.riley.s.eileen@gmail.com>
2022-07-12 19:45:52 +0630

    update

--- components/home/hero/index.jsx
+++ components/home/hero/index.jsx
@@ -29,14 +29,14 @@ const Hero = () => {
             <button className="grid p-1 border rounded-full place-items-center border-slate-700 text-slate-800">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                className="w-5 h-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                   strokeLinejoin="round"
                   d="M15 19l-7-7 7-7"
                 />
@@ -45,14 +45,14 @@ const Hero = () => {
             <button className="grid p-1 border rounded-full place-items-center border-slate-700 text-slate-800">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                className="w-5 h-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                   strokeLinejoin="round"
                   d="M9 5l7 7-7 7"
                 />
